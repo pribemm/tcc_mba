8b88d71a749a1bf39ae13694e037e05a702f4278
pribemm <111651041+pribemm@users.noreply.github.com>
2025-06-30 09:56:02 -0300

    Update salva_parquet.py

--- salva_parquet.py
+++ salva_parquet.py
@@ -7,13 +7,13 @@
 #Encontra o encoding do arquivo csv
 import chardet
 
-with open("G:\Meu Drive\ciencia de dados\Pos\TCC\TCC\dados\enem\microdados_enem_2023\DADOS\MICRODADOS_ENEM_2023.csv", 'rb') as f:
+with open("G://Meu Drive//ciencia de dados//Pos//TCC//TCC//dados//enem//microdados_enem_2023//DADOS//MICRODADOS_ENEM_2023.csv", 'rb') as f:
     result = chardet.detect(f.read(1000))  # Lê todo o arquivo para detectar a codificação
 
 print(result)  # Exibe a codificação detectada
 
 #Banco de dados real, completo
-df = pd.read_csv("G://Meu Drive//ciencia de dados//Pos//TCC//TCC//dados//enem//microdados_enem_2023//DADOS//MICRODADOS_ENEM_2023.csv", encoding= 'ISO-8859-1', sep=';')
+df = pd.read_csv("G://Meu Drive//ciencia de dados//Pos//TCC//TCC//dados//enem//microdados_enem_2023//DADOS//MICRODADOS_ENEM_2023.csv", encoding= result['encoding'], sep=';')
 
 #Salva df no formato parque com o nome enem_2023.parquet
 df.to_parquet('enem_2023.parquet')
