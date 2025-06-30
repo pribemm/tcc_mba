6a7a1a09b52e62d100f6e801c612c98f63146a64
pribemm <111651041+pribemm@users.noreply.github.com>
2025-06-30 09:47:58 -0300

    Update salva_parquet.py

--- salva_parquet.py
+++ salva_parquet.py
@@ -1,3 +1,9 @@
+# Arquivos Parquet, normalmente ocupa menos espaço em disco que o formato csv e, por isso, é indicado quando se lida com grande volume de dados.
+# Esse arquivo, especificamente, ocupa 1,65GB no formato csv e, 416 MB no formato parquet.
+# O formato parquet foi desenvolvido pelo Apache e o dados são salvos em colunas. Ao contrário do formato CSV, o formato parquet salva as colunas 
+# separadamente e carrega apenas as colunas que serão utilizadas. Esse formato é indicado para ambiente de big data e datalakes e, é compatível com 
+# Apache Spark, Hive, AWS Athena, Google BigQuery e Pandas.
+
 #Encontra o encoding do arquivo csv
 import chardet
 
